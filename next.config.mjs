ccd7d192c3e49baf7a8acc3e58e208d3bd099649
Carlos Gimenez <rodrigoaveiro09@gmail.com>
2026-09-21 10:08:22 -0300

    fix: ignore eslint in vercel and update nextjs version

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   devIndicators: {
     appIsrStatus: false,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   poweredByHeader: false,
   async headers() {
     return [{
